4fec2f54c7ec46e5a9b9d5fe4c5e33386ab22abe
MarceloMendesFatec <marcelojm.96@gmail.com>
2022-09-13 11:08:18 -0300

    calculadora Simples

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function calc(op){
 
     num1 = parseFloat(document.getElementById("num1").value)
     num2 = parseFloat(document.getElementById("num2").value)
-    let result = 0;
-    
+    var result = 0;
+
     switch(op)
     {
         case '+': result  = num1 + num2; break;
@@ -14,16 +14,16 @@ function calc(op){
 
     document.getElementById("output").value = result;
 
-    let newOutput = num1 + " " + op + " " + num2 + " = " +result;
+    var newOutput = num1 + " " + op + " " + num2 + " = " +result;
 
     document.getElementById("history").innerHTML += "<p>" + newOutput + "</p>";
     
-    let list = document.getElementById("history");
+    var list = document.getElementById("history");
     if(list.children.length > 10){
         document.getElementById("history").removeChild(list.childNodes[0]);    
     }
     
-    
+
 }
 
 
